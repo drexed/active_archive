b9b5253fa146e1330c02bd5149930fa4738de4c5
Juan Gomez <j.gomez@drexed.com>
2016-10-19 19:01:38 -0400

    fix railties

--- lib/active_archive.rb
+++ lib/active_archive.rb
@@ -1,7 +1,25 @@
 require 'rails'
 
-%w(version railtie settings table_definition methods scopes base).each do |file_name|
+%w(version settings table_definition methods scopes base).each do |file_name|
   require "active_archive/#{file_name}"
 end
 
 require 'generators/active_archive/install_generator'
+
+module ActiveArchive
+  class Railtie < ::Rails::Railtie
+
+    initializer 'active_archive' do |app|
+      ActiveArchive::Railtie.instance_eval do
+        [app.config.i18n.available_locales].flatten.each do |locale|
+          (I18n.load_path << path(locale)) if File.file?(path(locale))
+        end
+      end
+    end
+
+    def self.path(locale)
+      File.expand_path("../../config/locales/#{locale}.yml", __FILE__)
+    end
+
+  end
+end
